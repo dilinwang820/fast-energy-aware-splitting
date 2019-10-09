50680ef1bfe6d3b62e669e609e9cd382dea6bf31
Dilin Wang <dilinwang820@gmail.com>
2019-10-09 14:42:21 -0500

    Update reproduce.sh

--- mbv1_cifar100/reproduce.sh
+++ mbv1_cifar100/reproduce.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 
-python summary.py --dataset cifar100 --load ./saved_models/iclr20_fast_cifar100_0.pth.tar --model-name split_finetune
+python summary.py --dataset cifar100 --load ./saved_models/splitting_0.pth.tar --model-name split_finetune
 
 for((id=1;id<8;id++))
 do
-    python summary.py --dataset cifar100 --load ./saved_models/iclr20_fast_cifar100_${id}.pth.tar --model-name split_finetune --sp
+    python summary.py --dataset cifar100 --load ./saved_models/splitting_${id}.pth.tar --model-name split_finetune --sp
 done
 
